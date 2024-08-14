2d1725366643e37cde11a7eb09c7133dbe8c39aa
rafaelnapolitano <rafaelnv.prof@gmail.com>
2024-08-14 20:58:19 -0300

    feat: create User service

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/service/UserService.java
@@ -0,0 +1,36 @@
+package com.picpaysimplificado.service;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.util.concurrent.ExecutionException;
+
+//criar logica de negocio e metodos de validação
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        // validação de autorizacao para efetuar transacao, só os COMMUN podem
+        if(sender.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Usuarios do tipo Logista, não estam autorizados a efetuar transações");
+        }
+
+        // validação para ver se o User tem o saldo (amount) em seu saldo
+        if(sender.getBalance().compareTo(amount)< 0) {
+            throw new Exception("Saldo Insuficiente");
+        };
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.userRepository.findUserById(id).orElseThrow(() -> new Exception("Ususario não encontrado"));
+    }
+
+    public void saveUser(User user){
+        this.userRepository.save(user);
+    }
+}
