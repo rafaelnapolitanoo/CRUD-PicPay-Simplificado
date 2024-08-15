e56088b8e10246434f33c43ee4ba1e5727bd0f11
rafaelnapolitano <rafaelnv.prof@gmail.com>
2024-08-15 18:43:29 -0300

    feat: create UserControllerm, UserDTO, Modify User

--- picpaysimplificado/src/main/java/com/picpaysimplificado/domain/user/User.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -1,5 +1,6 @@
 package com.picpaysimplificado.domain.user;
 
+import com.picpaysimplificado.dtos.UserDTO;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -16,17 +17,33 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String firstName;
+
     private String lastName;
+
     // nao pode existir mais de um documento
     @Column(unique = true)
     private String document;
+
     @Column(unique = true)
     private String email;
+
     private String passWord;
+
     private BigDecimal balance;
+
     @Enumerated(EnumType.STRING)
     private UserType userType;
 
+
+    public User(UserDTO data) {
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.balance = data.balance();
+        this.userType = data.userType();
+        this.passWord = data.password();
+        this.email = data.email();
+    }
 
 }
