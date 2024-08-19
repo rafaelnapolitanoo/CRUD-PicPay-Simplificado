20d8652a2d203cbf788748be859b91ff0636ec23
rafaelnapolitano <rafaelnv.prof@gmail.com>
2024-08-19 20:22:53 -0300

    feat: crete ControllerExceptionHandler and ExecptionDTO

--- picpaysimplificado/src/main/java/com/picpaysimplificado/infra/ControllerExceptionHandler.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/infra/ControllerExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.picpaysimplificado.infra;
 
+import com.picpaysimplificado.dtos.ExecptionDTO;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,5 +10,20 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ControllerExceptionHandler {
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity threatDuplicationEntry
+    public ResponseEntity threatDuplicationEntry (DataIntegrityViolationException exception){
+        ExecptionDTO execptionDTO = new ExecptionDTO("Usuario já cadastrado",
+                "400");
+        return ResponseEntity.badRequest().body(execptionDTO);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404 (EntityNotFoundException exception){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity treatGeneralException(Exception exception) {
+        ExecptionDTO execptionDTO = new ExecptionDTO(exception.getMessage(),"500");
+        return ResponseEntity.internalServerError().body(execptionDTO);
+    }
 }
