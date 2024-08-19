5579fc415a66f621abbeabaa015e99f4f8de2161
rafaelnapolitano <rafaelnv.prof@gmail.com>
2024-08-19 13:35:38 -0300

    images

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/infra/ControllerExceptionHandler.java
@@ -0,0 +1,12 @@
+package com.picpaysimplificado.infra;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity threatDuplicationEntry
+}
--- picpaysimplificado/src/main/java/com/picpaysimplificado/service/TransactionService.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/service/TransactionService.java
@@ -1,5 +1,8 @@
 package com.picpaysimplificado.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.picpaysimplificado.domain.transaction.Transaction;
 import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.dtos.TransactionDTO;
@@ -15,9 +18,7 @@ import java.time.LocalDateTime;
 import java.util.Map;
 
 @Service
-public class
-TransactionService {
-
+public class TransactionService {
 
     @Autowired
     private UserService userService;
@@ -31,50 +32,52 @@ TransactionService {
     @Autowired
     private NotificationService notificationService;
 
-
-
     public Transaction createTransaction(TransactionDTO transaction) throws Exception {
         User sender = this.userService.findUserById(transaction.senderId());
-        User receiver = this.userService.findUserById(transaction.senderId());
+        User receiver = this.userService.findUserById(transaction.receiverId()); // Corrigido aqui
 
         userService.validateTransaction(sender, transaction.value());
 
         boolean isAuthorized = this.authorizeTransaction(sender, transaction.value());
-        if(!isAuthorized) {
+        if (!isAuthorized) {
             throw new Exception("Transação não autorizada");
         }
 
         Transaction newTransaction = new Transaction();
 
-        // setar os valores
+        // Setando os valores
         newTransaction.setAmount(transaction.value());
         newTransaction.setSender(sender);
         newTransaction.setReceiver(receiver);
         newTransaction.setTimestamp(LocalDateTime.now());
 
-        // transação e auteração de dinheiro entre uma conta e outra
+        // Atualizando o saldo dos usuários
         sender.setBalance(sender.getBalance().subtract(transaction.value()));
         receiver.setBalance(receiver.getBalance().add(transaction.value()));
 
-
-        //persistir dados
+        // Persistindo a transação e as mudanças de saldo
         this.repository.save(newTransaction);
         this.userService.saveUser(sender);
         this.userService.saveUser(receiver);
 
-        // usar o serviço de notificação
+        // Notificando os usuários
         this.notificationService.sendNotification(sender, "Transação realizada com sucesso");
         this.notificationService.sendNotification(receiver, "Transação recebida com sucesso");
 
         return newTransaction;
-
     }
-    public boolean authorizeTransaction(User sender, BigDecimal value) {
-        ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://util.devi.tools/api/v2/authorize" , Map.class);
 
-        if(authorizationResponse.getStatusCode() == HttpStatus.OK) {
-            String message = (String) authorizationResponse.getBody().get("message");
-            return "Autorizado".equalsIgnoreCase(message);
-        } else return false;
-    };
+    // Método authorizeTransaction no TransactionService
+    public boolean authorizeTransaction(User sender, BigDecimal value) {
+        ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://util.devi.tools/api/v2/authorize", Map.class);
+
+        if (authorizationResponse.getStatusCode() == HttpStatus.OK) {
+            Map<String, Object> data = (Map<String, Object>) authorizationResponse.getBody().get("data");
+            Boolean isAuthorized = (Boolean) data.get("authorization");
+            return Boolean.TRUE.equals(isAuthorized);
+        } else {
+            return false;
+        }
+    }
+
 }
